20269dfe9a06cb812498d511567de9c975c3980a
Mryule1401 <lehuy011406@gmail.com>
2025-04-20 00:05:18 +0930

    1

--- Game Products/Snake Game/script.js
+++ Game Products/Snake Game/script.js
@@ -1,13 +1,18 @@
 const board = document.getElementById('game-board');
 console.log(board)
 //define game variables
-let snake = [{ x:13, y: 13}];
+gridSize = 25
+let snake = [{ x:13  , y: 13}];
+let food = generatefood();
 let gameStarted = false
-
+let direction = 'right';
+let gameInterval;
+let gameSpeedDelay = 200;
 //draw game map, snake, food
 function draw(){
     board.innerHTML = '';
     drawSnake();
+    drawFood();
 }
 
 //Draw snake
@@ -33,10 +38,93 @@ function setPosition(element, position){
 }
 
 const beginBackground = document.getElementById('begin-background')
-document.addEventListener('keydown',(event)=>{
-    if (event.code == 'Space' && !gameStarted){
-        gameStarted = true;
-        beginBackground.style.display ='none';
+function startGame(){
+    gameStarted = true;
+    beginBackground.style.display ='none';
+    gameInterval=setInterval(()=>{
+        moving();
         draw();
+    },gameSpeedDelay)
+}
+
+//keypress event listener
+function handleKeyPress(event){
+    if ((event.code == 'Space' && !gameStarted) || (event.code == ' ' && !gameStarted)){
+        startGame()
     }
-})
+    else{
+        switch (event.key){
+            case 'ArrowUp':
+            case 'w':
+                if (direction != 'down'){
+                    direction = 'up'; 
+                } 
+                break;
+            case 'ArrowDown':
+            case 's':
+                if (direction != 'up'){
+                    direction = 'down';
+                } 
+                break;
+            case 'ArrowLeft':
+            case 'a':
+                if (direction != 'right'){
+                    direction = 'left';
+                } 
+                break;
+            case 'ArrowRight':
+            case 'd':
+                if (direction != 'left'){
+                    direction = 'right';
+                }
+                break;
+        }
+    }
+}
+document.addEventListener('keydown',handleKeyPress)
+//making food
+function drawFood(){
+    const foodElement = createGameElement('div', 'food');
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+}
+
+//generating food location
+function generatefood(){
+    const x = Math.floor(Math.random()*gridSize)+1;
+    const y = Math.floor(Math.random()*gridSize)+1;
+    return {x,y};
+}
+
+//moving the snake
+function moving(){
+    const head = {...snake[0]};
+    switch (direction){
+        case 'right':
+            head.x++;
+            break;
+        case 'up':
+            head.y--;
+            break;
+        case 'left':;
+            head.x--
+            break;
+        case 'down':
+            head.y++;
+            break;
+    }
+
+    snake.unshift(head);
+    // snake.pop();
+    if (head.x==food.x && head.y == food.y){
+        food = generatefood();
+        clearInterval();
+        gameInterval = setInterval(()=>{
+            moving();
+            draw();
+        },gameSpeedDelay);
+    } else {
+        snake.pop();
+    }
+}
+
